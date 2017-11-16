7561d248564438e1afe2c0e109684d03044c07fb
Nick Leake <leakend@dukes.jmu.edu>
2017-11-15 19:53:29 -0500

    refactor grid and add reset button

--- js/app.js
+++ js/app.js
@@ -5,9 +5,23 @@ $(document).ready(function() {
             unit.appendTo('.container');
         }
     }
-    
+
 
     $('.unit').hover(function(){
     	$(this).addClass('black');
     });
+
+
+    $('#reset').on('click', function(){
+      $('.unit').remove();
+      newBoard = prompt('How many squares per side would you like?', 16)
+      //if (newBoard !== )
+      //generate new board
+      for (var x = 0; x < newBoard; x++){
+        for(var y = 0; y < newBoard; y++){
+          var unit = $("<div class='unit'></div>");
+          unit.appendTo('.container');
+        }
+      }
+    })
 });
